49e69e805acf8d87072925c56c92c7f0ca422ae9
Dave Arnold <darnold@hashicorp.com>
2018-06-24 16:39:09 -0700

    fixing vault_address

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "nomad_cluster" {
   subnet          = "${data.terraform_remote_state.network.public_subnet}"
   vpc_id          = "${data.terraform_remote_state.network.vpc_id}"
   consul_cluster  = "${data.aws_instance.consul_instance.private_ip}"
-  vault_cluster   = "${data.terraform_remote_state.vault_cluster.address}"
+  vault_cluster   = "${data.terraform_remote_state.vault_cluster.vault_address}"
   service_name    = "nomad"
   service_version = "1.0.0"
 }
