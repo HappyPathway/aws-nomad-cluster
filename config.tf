70e1a5e15d9967611efff521aa87f68337eff1b1
Dave Arnold <darnold@hashicorp.com>
2018-06-25 13:59:03 -0700

    removing security groups and consul instance

--- config.tf
+++ config.tf
@@ -25,10 +25,6 @@ data "terraform_remote_state" "consul_cluster" {
   }
 }
 
-data "aws_instance" "consul_instance" {
-  instance_id = "${data.terraform_remote_state.consul_cluster.instances[0]}"
-}
-
 data "terraform_remote_state" "vault_cluster" {
   backend = "atlas"
 
