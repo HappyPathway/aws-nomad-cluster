50a5a39ded8636504f1c0a39657e82bb3684781d
Dave Arnold <darnold@hashicorp.com>
2018-06-25 21:25:00 -0700

    bump

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 
 module "nomad_cluster" {
   source            = "app.terraform.io/Darnold-Hashicorp/nomad-cluster/aws"
-  version           = "1.3.9"
+  version           = "1.4.0"
   key_name          = "${var.key_name}"
   servers           = "${var.servers}"
   clients           = "${var.clients}"
